2ecae986771186bf58974c4fb78c00e919c63e0a
Phasathorn Suwansri <arbacuz@gmail.com>
2017-01-12 16:40:58 +0700

    Update ex01

--- 01-assignment/src/main/java/main.java
+++ 01-assignment/src/main/java/main.java
@@ -1,10 +1,33 @@
-/**
- * Created by lukkiddd on 1/12/2017 AD.
- */
 public class main {
+    private static int top = 0;
+    private static int st[] = new int[10];
 
     public static void main(String args[]) {
-        System.out.print("Hello World");
+        int j;
+
+        for(j = 0; j <= 10; j++) {
+            push(j + 1);
+        }
+        for(j = 0; j <= 10; j++) {
+            System.out.println(pop());
+        }
+    }
+
+    private static void push(int x) {
+        System.out.println(st.length);
+
+        if(top >= st.length) {
+            System.out.println("Stack is full");
+        } else {
+            st[top++] = x;
+        }
+    }
+
+    private static int pop() {
+        if(top > 0)
+            return st[--top];
+        System.out.println("Stack under flow");
+        return 0;
     }
 
 }
